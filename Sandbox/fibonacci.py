ccd0c4f4bdbf0e261434b327685f52703c995079
Yaiba782 <yaiba782@gmail.com>
2015-10-28 17:17:30 +0100

    adding some stuff to fibonacci

--- Sandbox/fibonacci.py
+++ Sandbox/fibonacci.py
@@ -5,5 +5,8 @@ def fibonacci(n):
         return 1
     return fibonacci(n-1)+fibonacci(n-2)
 while True :
-    n = int(input('Entrez le nombre inférieur à 50 que vous voulez \n (pas au dessus, '))
-    print(fibonacci(n))
+    n = int(input('Entrez le nombre inférieur à 50 que vous voulez \n(pas au dessus, sinon ça va chauffer le CPU)\n'))
+    if n<50 :
+        print(fibonacci(n))
+    else:
+        print('Nombre trop grand')
